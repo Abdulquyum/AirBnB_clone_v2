93b4e373145bd51fe2cc1146424101a824c368f0
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-16 23:34:22 +0100

    Update User info in the database

--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel
 from sqlachemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
