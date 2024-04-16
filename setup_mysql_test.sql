e9cf3e5f1391942ab96f486aa3e8a66cc4336424
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-16 14:03:47 +0100

    prepares a MySQL server hbnb_test_db for AirBnB_clone_v2 project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,6 @@
 -- Prepares a MySQL server for user hbnb_test and hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFY BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON performance_schema.* TO 'hbhb_test'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
