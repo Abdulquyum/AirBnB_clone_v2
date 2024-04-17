3cfe94560a7392fb4b3515a7abc57a92dea0e554
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-04-17 18:51:21 +0100

    changing to DataBase storage type

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,11 +13,16 @@ class DBStorage:
     __session = None
 
     def __init__(self):
+        """ SQLAlchemy engine initiator """
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                 format(getenv(BNB_MYSQL_USER), getenv(HBNB_MYSQL_PWD),
                        getenv(HBNB_MYSQL_HOST), getenv(HBNB_MYSQL_DB)),
                        pool_pre_ping=True)
 
     def drop_tables(self):
+        """
+        Drops all tables 
+        if the environment variable HBNB_ENV is equal to test
+        """
         metadata = Metadata(bind=self.__engine)
         metadata.drop_all()
