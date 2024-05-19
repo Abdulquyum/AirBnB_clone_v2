7bc14d1b86999193145d4883af76e44780157fd4
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-05-20 00:21:31 +0100

    starts engine and route for /c/<text>

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" /c/<text>: display “C ” followed by the value of the text """
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def slash():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_slash():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slash=False)
+def text_slash():
+    return f"C {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
